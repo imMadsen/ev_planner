5b349c2722bfbc272a911d1c36c638178bf59e50
Rasmus Madsen <76999316+imMadsen@users.noreply.github.com>
2024-08-02 19:57:28 +0200

    Update k_nearest.ts

--- client/src/prune/k_nearest.ts
+++ client/src/prune/k_nearest.ts
@@ -1,31 +1,31 @@
 import { type ChargingStation } from "../chargemap";
 import {
-  distancePointToLineSegmentInKM,
+  distancePointToLineSegment,
 } from "../utilities";
 
-export function k_nearest(chargingStations: ChargingStation[], verticies: number[][], maxRange: number) {
-  const prunedChargingStations: ChargingStation[] = [];
-  let previousNode: number[] | undefined;
+export function k_nearest(chargingStations: ChargingStation[], verticies: number[][], k: number) {
+  let prunedChargingStations: ChargingStation[] = [];
+  // Using a Subset for dev purposes
 
+  let previousNode: number[] | undefined;
   verticies.forEach((node, index) => {
     const [lng1, lat1] = node;
-
     if (previousNode !== undefined && index > 0) {
       const [lng2, lat2] = previousNode;
-      chargingStations.forEach((chargingStation) => {
-        const dist = distancePointToLineSegmentInKM(
-          chargingStation.lat,
-          chargingStation.lng,
-          lat1,
-          lng1,
-          lat2,
-          lng2
-        );
+      const nearestChargingStations: { distance: number, chargingStation: ChargingStation }[] = [];
 
-        if (dist <= maxRange) {
-          prunedChargingStations.push(chargingStation);
-        }
+      chargingStations.forEach((chargingStation) => {
+        const dist = distancePointToLineSegment(chargingStation.lat, chargingStation.lng, lat1, lng1, lat2, lng2);
+        nearestChargingStations.push({ distance: dist, chargingStation });
       });
+
+      // Sort the nearest charging stations by distance
+      nearestChargingStations.sort((a, b) => a.distance - b.distance);
+
+      // Get the first k closest charging stations
+      const closestStationsForSegment = nearestChargingStations.slice(0, k).map(entry => entry.chargingStation);
+
+      prunedChargingStations = [...prunedChargingStations, ...closestStationsForSegment];
     }
 
     previousNode = node;
